568a203cfed062e316370d0c25bb0e7d7747d91a
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-10-20 22:10:15 -0300

    homepage rodando ao ir para a rota '/'

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home/Home.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
   }
 ])
 
