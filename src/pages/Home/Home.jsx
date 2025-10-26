bd85960b94a607234377ee3f4b6c74ca11498f29
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-10-25 23:41:44 -0300

    inputs de modo de jogo e de quantidade de cartas criados e funcionando através de useState

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 
 
 function Home(){
+    //useStates para quantidade de cartas e modo de jogo
+    const [qntdCartas, setQntdCartas] = useState('');
+    const [modoJogo, setModoJogo] = useState('');
+
+    //funções para alterar modo de jogo e quantidade de cartas
+    const alteraModoJogo = (event) => {
+        setModoJogo(event.target.value);
+    }
+    const alteraQntdCartas = (event) => {
+        setQntdCartas(event.target.value);
+    }
 
     return (
         <main>
@@ -9,30 +21,79 @@ function Home(){
             <h4>Escolha quantas cartas vai ter no jogo</h4>
             <ul>
                 <li>
+                    <label>
+                        <input 
+                            type="radio"
+                            value="dez"
+                            checked={qntdCartas === 'dez'}
+                            onChange={alteraQntdCartas} />   
 
+                            dez cartas
+                    </label>
                 </li>
                 <li>
+                    <label>
+                        <input 
+                            type="radio"
+                            value="dezoito"
+                            checked={qntdCartas === 'dezoito'}
+                            onChange={alteraQntdCartas} />   
 
+                            dezoito cartas
+                    </label>
                 </li>
                 <li>
+                    <label>
+                        <input 
+                            type="radio"
+                            value="vinte"
+                            checked={qntdCartas === 'vinte'}
+                            onChange={alteraQntdCartas} />   
 
+                            vinte cartas
+                    </label>
                 </li>
             </ul> { /* lista de qntd de carta */ }
 
             <h4>Escolha o modo de jogo</h4>
             <ul> 
                 <li>
-                    
+                    <label>
+                        <input 
+                        type="radio"
+                        value="sozinho"
+                        checked={modoJogo === 'sozinho'}
+                        onChange={alteraModoJogo} />
+
+                        Sozinho
+                    </label>
                 </li>
                 <li>
-                    
+                    <label>
+                        <input 
+                        type="radio"
+                        value="vsMaquina"
+                        checked={modoJogo === 'vsMaquina'}
+                        onChange={alteraModoJogo} />
+
+                        Contra Máquina
+                    </label>
                 </li>
                 <li>
-                    
+                    <label>
+                        <input 
+                        type="radio"
+                        value="vsPlayer"
+                        checked={modoJogo === 'vsPlayer'}
+                        onChange={alteraModoJogo} />
+
+                        Contra player
+                    </label>
                 </li>
             </ul> { /* lista dos modos de jogo*/ }
 
-            <button disabled="true">Iniciar jogo</button>
+            { /* botão apenas ativa quando o modo de jogo e a quantidade de cartas estiverem setadas */ }
+            <button disabled={ !(modoJogo && qntdCartas) }>Iniciar jogo</button>
         </main>
     )
 }
