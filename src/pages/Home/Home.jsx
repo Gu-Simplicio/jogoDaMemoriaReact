d06a81699ff597effb4449c14a56535268357d4b
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-10-26 01:36:19 -0300

    inputs retirados

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,38 @@
+
+
 function Home(){
+
     return (
         <main>
-            
+            <h1>Bem vindo ao jogo da memória!</h1>
+
+            <h4>Escolha quantas cartas vai ter no jogo</h4>
+            <ul>
+                <li>
+
+                </li>
+                <li>
+
+                </li>
+                <li>
+
+                </li>
+            </ul> { /* lista de qntd de carta */ }
+
+            <h4>Escolha o modo de jogo</h4>
+            <ul> 
+                <li>
+                    
+                </li>
+                <li>
+                    
+                </li>
+                <li>
+                    
+                </li>
+            </ul> { /* lista dos modos de jogo*/ }
+
+            <button disabled="true">Iniciar jogo</button>
         </main>
     )
 }
