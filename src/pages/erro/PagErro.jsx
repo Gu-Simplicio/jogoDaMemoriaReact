287fb9cdc0fd1760b10fa0b95614719922ace544
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-10-25 23:54:05 -0300

    página de erro criada

---
+++ src/pages/erro/PagErro.jsx
@@ -0,0 +1,13 @@
+function PagErro(){
+    return (
+        <div>
+            <h1>Essa rota não existe..</h1>
+
+            <a href="/">
+                Clique aqui para ir para HomePage
+            </a>
+        </div>
+    )
+}
+
+export default PagErro;
