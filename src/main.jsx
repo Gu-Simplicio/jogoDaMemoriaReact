6126216fd5e7d3e308b32334edcc21484970dfd9
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-10-26 00:13:29 -0300

    página do jogo criado

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home/Home.jsx'
 import PagErro from './pages/erro/PagErro.jsx'
+import Jogo from './pages/jogo/Jogo.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    errorElement: <PagErro />
+    errorElement: <PagErro />,
+  },
+  {
+    path: '/jogo',
+    element: <Jogo />
   }
 ])
 
