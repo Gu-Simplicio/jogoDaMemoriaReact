772f0c47e0f21a1cf19b8a02173fc1592dc7fbc4
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-11-08 20:54:20 -0300

    lista de card comtítulos diferentes

--- src/pages/jogo/Jogo.jsx
+++ src/pages/jogo/Jogo.jsx
@@ -1,12 +1,32 @@
+import cards from "../../config/dadosCard.json"
 import Card from "../../components/Card/Card";
 
+//função para carregar os dados de configuração do jogo
 function carregaConfig(){
+    //caso não tenha nenhum dado configurado no localStorage
     if(!localStorage.getItem("qntdCartas") || !localStorage.getItem("modoJogo")) return false;
 
+    //recebe e retorna as configurações
     const qntdCartas = localStorage.getItem("qntdCartas");
     const modoJogo = localStorage.getItem("modoJogo");
 
     return { qntdCartas, modoJogo }
+}
+//configura a lista de cards
+function configuraListaCards() {
+    let listaCards = new Array()
+    listaCards = [...cards, ...cards];
+
+    //loop para embaralhar a lista 
+    for(let i = listaCards.length - 1; i > 0; i--){
+        //escolhe um índice aleatório entre 0 e i
+        const j = Math.floor(Math.random() * (i + 1));
+
+        //troca o elemento atual pelo aleatório
+        [ listaCards[i], listaCards[j] ] = [ listaCards[j], listaCards[i] ];
+    }
+    
+    return listaCards;
 }
 
 function Jogo(){
@@ -18,8 +38,11 @@ function Jogo(){
         location.href = "/";
     }
 
+    //caso algo seja errado
     if(!qntdCartas || !modoJogo || !carregaConfig()) retornaHome();
 
+    const listaCards = configuraListaCards();
+    console.table(listaCards);
 
     return(
         <main>
