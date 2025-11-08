d9564abbad8b1c2e0ae721d84cb5329d4e1c6e6d
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-11-08 20:17:46 -0300

    código do componente do card criado

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,24 @@
-function Card() {
+import { useRef } from "react";
+
+function Card({ idCard, imgCard, tituloCard }) {   
+    //cria uma referência que começa nula
+    let classeAtual = useRef(null);
+
+    //função que troca a classe do elemento referenciado
+    const trocaClasse = () => {
+        classeAtual.current.classList.toggle("escondido");
+    }
+    
     return (
-        <h1>Card funcionando!</h1>
+        <div 
+            id={idCard} 
+            ref={classeAtual}
+            onClick={() => trocaClasse()}>
+            <img 
+                src={imgCard} 
+                alt={`Card com a imagem ${imgCard}`} />
+            <p> { tituloCard } </p>
+        </div>
     )
 }
 
